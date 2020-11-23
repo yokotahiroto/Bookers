071904b43325eeaca98ada987302653d46eb0b02
yokotahiroto <hy_dkbs66mh@icloud.com>
2020-11-23 11:47:57 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    redirect_to
+  end
+
+  def create
+     @book = Book.new(book_params)
+     if @book.save
+     redirect_to @book, notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+     render :index
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to book
+  end
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'top#index'
+  get '/books', to: 'books#index'
+  post 'books' => 'books#create'
+  get '/books/:id', to: 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+end
